1f68f48898872ffa82a59f2e05e3a63572adf62d
Nikki Wu <nikkiwu9@gmail.com>
2019-09-13 19:26:53 -0700

    Updates

---
+++ precache-manifest.210f6244f1870acd7fd633f5050d73cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2a0616c96409778a56e254872e98d0a",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "5658ddafcb89726bd646",
+    "url": "/pokedex/static/css/main.e417c085.chunk.css"
+  },
+  {
+    "revision": "6b9bad561f4921a145c9",
+    "url": "/pokedex/static/js/2.777c2879.chunk.js"
+  },
+  {
+    "revision": "5658ddafcb89726bd646",
+    "url": "/pokedex/static/js/main.2b07f52d.chunk.js"
+  },
+  {
+    "revision": "76a9955e03d38b59391b",
+    "url": "/pokedex/static/js/runtime~main.a216a149.js"
+  },
+  {
+    "revision": "6a398990b1d7b6c11135bd5823555f99",
+    "url": "/pokedex/static/media/sprites.6a398990.png"
+  }
+]);
